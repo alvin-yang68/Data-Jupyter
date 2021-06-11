876441930db9a77ca373d96ddad6c7873fd6bb74
alvin-yang68 <alvin.yang68@gmail.com>
2021-06-11 17:58:16 +1200

    Added all files for heroku

--- backend/index/index.py
+++ backend/index/index.py
@@ -9,8 +9,3 @@ index_bp = Blueprint('index_bp', __name__)
 @index_bp.route('/index')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
-
-
-@index_bp.errorhandler(404)
-def not_found(e):
-    return send_from_directory(app.static_folder, 'index.html')
