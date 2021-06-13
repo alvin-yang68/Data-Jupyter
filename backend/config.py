8f5cb076adee615add08c87e99fc438d93735967
alvin-yang68 <alvin.yang68@gmail.com>
2021-06-13 19:45:12 +1200

    Heroku deployment

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    MONGO_URI = os.environ.get(
-        'MONGODB_URI') or 'mongodb+srv://Alvin:R2y7sefqZJL9cXp0@cluster0.xhr3k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
