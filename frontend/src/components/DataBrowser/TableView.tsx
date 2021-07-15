0b393a5f3936ab789defbadc94d72c89095e6513
alvin-yang68 <alvin.yang68@gmail.com>
2021-07-15 12:35:24 +1200

    Frontend: Added virtual table to the TableView of Data Browser

--- frontend/src/components/DataBrowser/TableView.tsx
+++ frontend/src/components/DataBrowser/TableView.tsx
@@ -1,18 +1,62 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import ReactMarkdown from 'react-markdown';
-import gfm from 'remark-gfm';
+import React from "react";
+import { shallowEqual, useSelector } from "react-redux";
 
-import { AppState } from '../../store';
+import { AppState } from "../../store";
+import { loadRows } from "../../api/notebook";
+import VirtualTable, { Data } from "../generics/VirtualTable";
 
 export default function TableView(): React.ReactElement {
-  const tableData = useSelector<AppState, string>((state) => (state.browser.table));
+  const rowCount = useSelector<AppState, number>(
+    (state) => state.browser.tableRowCount
+  );
+
+  const headers = useSelector<AppState, string[]>(
+    (state) => state.browser.tableHeaders,
+    shallowEqual
+  );
+
+  /** The Row component. This should be a table row.
+   *  Note that we don't use the `style` that regular `react-window` examples pass in. */
+  const renderRows = ({
+    index,
+    data,
+  }: {
+    index: number;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    data: Data<Record<string, any>>;
+  }) => {
+    const row = data.get(index);
+
+    return (
+      <tr>
+        {/** Make sure table rows are the same height as what was passed into the list... */}
+        {headers.map((header) => (
+          <td style={{ height: "36px" }} key={header}>
+            {row ? row[header] : "Loading..."}
+          </td>
+        ))}
+      </tr>
+    );
+  };
 
   return (
-    <div className="px-4 w-full flex-grow bg-gray-50 border border-gray-300 block whitespace-nowrap overflow-scroll">
-      <ReactMarkdown remarkPlugins={[gfm]}>
-        {tableData}
-      </ReactMarkdown>
-    </div>
+    <VirtualTable
+      height={300}
+      width="100%"
+      itemCount={rowCount}
+      itemSize={36}
+      loadItems={loadRows}
+      header={
+        // Make the header sticky.
+        <thead style={{ position: "sticky", top: 0 }}>
+          <tr>
+            {headers.map((header) => (
+              <th>{header}</th>
+            ))}
+          </tr>
+        </thead>
+      }
+      row={renderRows}
+    />
   );
 }
---
+++ frontend/src/components/generics/VirtualTable.tsx
@@ -0,0 +1,148 @@
+import React, { useContext, useState, useRef, useCallback } from "react";
+import { FixedSizeList, FixedSizeListProps } from "react-window";
+import InfiniteLoader from "react-window-infinite-loader";
+import BTree from "sorted-btree";
+
+import { range } from "../../utils";
+
+type VirtualTableState = {
+  top: number;
+  setTop: (top: number) => void;
+  header: React.ReactNode;
+  footer: React.ReactNode;
+};
+
+/** Context for cross component communication */
+const VirtualTableContext = React.createContext<VirtualTableState>({
+  top: 0,
+  setTop: () => {
+    // Do nothing
+  },
+  header: <></>,
+  footer: <></>,
+});
+
+/**
+ * The `Inner` component of the virtual list. This is the "Magic".
+ * Capture what would have been the top elements position and apply it to the table.
+ * In other words, it vertically translates the table instead of scrolling over the table.
+ * Other than that, render an optional header and footer.
+ * */
+const Inner = React.forwardRef<HTMLDivElement, React.HTMLProps<HTMLDivElement>>(
+  ({ children, ...rest }: React.ComponentProps<"div">, ref) => {
+    const { header, footer, top } = useContext(VirtualTableContext);
+    return (
+      <div {...rest} ref={ref}>
+        <table style={{ top, position: "absolute", width: "100%" }}>
+          {header}
+          <tbody>{children}</tbody>
+          {footer}
+        </table>
+      </div>
+    );
+  }
+);
+
+export type Data<T> = BTree<number, T | undefined>;
+
+interface IProps<T> {
+  header?: React.ReactNode;
+  footer?: React.ReactNode;
+  loadItems: (startIndex: number, stopIndex: number) => Promise<T[]>;
+  row: FixedSizeListProps["children"];
+  minimumBatchSize?: number;
+  threshold?: number;
+}
+
+/**
+ * The virtual table. It basically accepts all of the same params as the original `FixedSizeList`.
+ */
+export default function VirtualTable<T>({
+  row,
+  header,
+  footer,
+  loadItems,
+  threshold = 15,
+  ...rest
+}: IProps<T> &
+  Omit<
+    FixedSizeListProps,
+    "children" | "innerElementType" | "onItemsRendered" | "ref"
+  >): React.ReactElement {
+  const listRef = useRef<FixedSizeList | null>();
+  const [top, setTop] = useState(0);
+
+  // Items are stored in a sorted B-tree. The key is item index.
+  // The value is either the item data or `undefined`, which means the item
+  // has been loaded (i.e. rendered on screen), but its data has not arrived
+  // from the API yet.
+  const [data] = useState<Data<T>>(() => new BTree<number, T | undefined>());
+
+  const isItemLoaded = (index: number): boolean => data.has(index);
+
+  const loadMoreItems = useCallback(
+    async (startIndex: number, stopIndex: number): Promise<void> => {
+      // Clear any items where their indexes are below `threshold` from the
+      // loaded items (visible on screen) to save space.
+      if (startIndex - threshold > 0)
+        data.deleteRange(0, startIndex - threshold, false);
+
+      // Clear any items where their indexes are above `threshold` from the
+      // loaded items (visible on screen) to save space.
+      const maxKey = data.maxKey() || 0;
+      if (stopIndex + threshold < maxKey)
+        data.deleteRange(stopIndex + threshold, maxKey, true);
+
+      // Temporarily set the values of loaded items to `undefined` (waiting for API to populate).
+      range(startIndex, stopIndex + 1).forEach((index) => {
+        data.set(index, undefined);
+      });
+
+      // Request the contents of items between `startIndex` and `stopIndex` from the API.
+      const contents = await loadItems(startIndex, stopIndex);
+
+      // Replace `undefined` values with contents.
+      contents.forEach((content, index) => {
+        data.set(index + startIndex, content);
+      });
+    },
+    []
+  );
+
+  return (
+    <VirtualTableContext.Provider value={{ top, setTop, header, footer }}>
+      <InfiniteLoader
+        {...rest}
+        isItemLoaded={isItemLoaded}
+        loadMoreItems={loadMoreItems}
+      >
+        {({ onItemsRendered, ref }) => (
+          <FixedSizeList
+            {...rest}
+            itemData={data}
+            innerElementType={Inner}
+            onItemsRendered={(props) => {
+              const style =
+                listRef.current &&
+                // @ts-ignore private method access
+                // eslint-disable-next-line no-underscore-dangle
+                listRef.current._getItemStyle(props.overscanStartIndex);
+              setTop((style && style.top) || 0);
+
+              // Call the callback provided by `InfiniteLoader`
+              onItemsRendered(props);
+            }}
+            ref={(el) => {
+              listRef.current = el;
+
+              // Pass in `ref` to the callback provided by `InfiniteLoader`
+              if (typeof ref === "function") ref(el);
+            }}
+          >
+            {row}
+          </FixedSizeList>
+        )}
+      </InfiniteLoader>
+    </VirtualTableContext.Provider>
+  );
+}
